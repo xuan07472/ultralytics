76c181459198ad4bd27ac0c2e3cd232a16370397
xuan07472 <xuan07472@gmail.com>
2024-06-24 01:31:56 +0800

    111

--- val_c.py
+++ val_c.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     # 
-    model = YOLO('/home/ubuntu/guidang/ultralytics/runs/train/b150n-C2f_SCConv_b2_Triplet_v1_c2/weights/best.pt')
+    model = YOLO('/home/ubuntu/guidang/ultralytics/runs/train/b150n-C2f_SCConv_b2_Triplet_v1_c/weights/best.pt')
     # model = YOLO('/home/ubuntu/ultralytics/runs/prune/yolov8_b150n_C2f_SCConv_b2_Triplet_v1_lamp200_s2ng-prune/weights/prune.pt')
     model.val(data='/home/ubuntu/guidang/ultralytics/datasets/luderick_base/luderick_base.yaml',
               split='val',
@@ -14,6 +14,6 @@ if __name__ == '__main__':
               # save_json=True, # if you need to cal coco metrice
               # project='runs/val/2080ti',
               project='runs/val/',
-              name='b150n-C2f_SCConv_b2_Triplet_v1_c16',
+              name='b150n-C2f_SCConv_b2_Triplet_v1_c',
               )
 
