7246816ee67c2cb31861337a85c235e06cba1599
xuan07472 <xuan07472@gmail.com>
2024-06-24 08:43:27 +0800

    111

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 if __name__ == '__main__':
     model = YOLO('ultralytics/ultralytics/cfg/models/v8/yolov8_C2f_SCConv_b2_Triplet_v1_c.yaml')
     # model = YOLO('/home/ubuntu/guidang/ultralytics/ultralytics/cfg/models/v8/yolov8.yaml')
-    model.train(data='/ultralytics/datasets/luderick_base/luderick_base.yaml',
+    model.train(data='ultralytics/datasets/luderick_base/luderick_base.yaml',
                 cache=False,
                 imgsz=640,
                 epochs=150,
